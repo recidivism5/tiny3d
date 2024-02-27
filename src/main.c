57e66684ecfb1af0dd3284557f02ae61fa6324c7
Ian Bryant <ijb2020syr@gmail.com>
2024-02-27 07:58:51 -0500

    copy framebuffer

--- src/main.c
+++ src/main.c
@@ -1,111 +1,17 @@
 #include <base.h>
 #include <stb_image.h>
 
-typedef struct {
-	uint8_t r,g,b,a;
+typedef union {
+	struct {
+		uint8_t r,g,b,a;
+	};
+	uint32_t w;
 } Color;
 
-typedef struct {
-	int width, height, pitch;
-	Color *pixels;
-} Image;
-
 //globals:
-Image screen = {
-	.width = 256,
-	.height = 192
-};
-
-Image atlas;
-
-//code:
-
-void load_image(Image *t, char *name){
-	int channels;
-	t->pixels = (Color *)stbi_load(local_path_to_absolute("res/textures/%s",name),&t->width,&t->height,&channels,4);
-	t->pitch = t->width*sizeof(*t->pixels);
-}
-
-#define PIXEL_AT(img,x,y) (((Color *)((char *)(img)->pixels+(y)*(img)->pitch))[(x)])
-
-void blit8(Image *src, Image *dst, int sx, int sy, int dx, int dy, int rotation, bool xflip){
-	if (dx < -7 || dy < -7){
-		return;
-	}
-	int xo = MAX(-dx,0);
-	int yo = MAX(-dy,0);
-	int w = MIN(dst->width-dx,8);
-	if (w <= 0){
-		return;
-	}
-	int h = MIN(dst->height-dy,8);
-	if (h <= 0){
-		return;
-	}
-	if (xflip){
-		switch (rotation){
-			case 0:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+7-x,sy*8+y);
-					}
-				}
-				break;
-			case 1:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+y,sy*8+x);
-					}
-				}
-				break;
-			case 2:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+x,sy*8+7-y);
-					}
-				}
-				break;
-			case 3:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+7-y,sy*8+7-x);
-					}
-				}
-				break;
-		}
-	} else {
-		switch (rotation){
-			case 0:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+x,sy*8+y);
-					}
-				}
-				break;
-			case 1:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+7-y,sy*8+x);
-					}
-				}
-				break;
-			case 2:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+7-x,sy*8+7-y);
-					}
-				}
-				break;
-			case 3:
-				for (int y = yo; y < h; y++){
-					for (int x = xo; x < w; x++){
-						PIXEL_AT(dst,dx+x,dy+y) = PIXEL_AT(src,sx*8+y,sy*8+7-x);
-					}
-				}
-				break;
-		}
-	}
-}
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 192
+Color screen[SCREEN_WIDTH*SCREEN_HEIGHT];
 
 int SDL_main(int argc, char* argv[]){
 	SDL_ASSERT(!SDL_Init(SDL_INIT_EVERYTHING));
@@ -128,18 +34,16 @@ int SDL_main(int argc, char* argv[]){
 		SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC
 	);
 	SDL_ASSERT(renderer);
-	SDL_SetWindowMinimumSize(window,screen.width,screen.height);
-	SDL_ASSERT(!SDL_RenderSetLogicalSize(renderer,screen.width,screen.height));
+	SDL_SetWindowMinimumSize(window,SCREEN_WIDTH,SCREEN_HEIGHT);
+	SDL_ASSERT(!SDL_RenderSetLogicalSize(renderer,SCREEN_WIDTH,SCREEN_HEIGHT));
 	SDL_ASSERT(!SDL_RenderSetIntegerScale(renderer,true));
 	SDL_Texture *screenTexture = SDL_CreateTexture(
 		renderer,
 		SDL_PIXELFORMAT_RGBA32,
 		SDL_TEXTUREACCESS_STREAMING,
-		screen.width,
-		screen.height
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT
 	);
-
-	load_image(&atlas,"atlas.png");
 
 	bool quit = false;
 
@@ -174,19 +78,10 @@ int SDL_main(int argc, char* argv[]){
 
 		SDL_RenderClear(renderer);
 
-		SDL_LockTexture(screenTexture,0,&screen.pixels,&screen.pitch);
-		for (int y = 0; y < screen.height; y++){
-			for (int x = 0; x < screen.width; x++){
-				PIXEL_AT(&screen,x,y) = (Color){0,0,0,255};
-			}
-		}
-		for (int i = 0; i < 4; i++){
-			blit8(&atlas,&screen,0,2,mx+9*i,my,i,false);
-		}
-		for (int i = 0; i < 4; i++){
-			blit8(&atlas,&screen,0,2,mx+9*i,my+8,i,true);
-		}
-		SDL_UnlockTexture(screenTexture);
+		screen[0].r = 255;
+		screen[0].a = 0;
+
+		SDL_UpdateTexture(screenTexture,0,screen,SCREEN_WIDTH*sizeof(*screen));
 
 		SDL_RenderCopy(renderer,screenTexture,0,0);
 
