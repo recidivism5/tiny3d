9c46fe22f271221f622f239ae14d61ccdf47e6e6
mf <fucker@pornhub.com>
2024-03-27 09:10:08 -0400

    fix weird ass indentation in mac.m

--- src/platform/mac.m
+++ src/platform/mac.m
@@ -8,65 +8,65 @@
 @end
 @implementation MyOpenGLView
 - (void)drawRect:(NSRect)dirtyRect {
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_QUADS);
-    glVertex2f(0,0);
-    glVertex2f(1,0);
-    glVertex2f(1,1);
-    glVertex2f(0,1);
-    glEnd();
-    [[self openGLContext] flushBuffer];
+   glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+   glClear(GL_COLOR_BUFFER_BIT);
+   glBegin(GL_QUADS);
+   glVertex2f(0,0);
+   glVertex2f(1,0);
+   glVertex2f(1,1);
+   glVertex2f(0,1);
+   glEnd();
+   [[self openGLContext] flushBuffer];
 }
 @end
 
 @interface AppDelegate : NSObject <NSApplicationDelegate> {
-    NSWindow* window;
-    MyOpenGLView* glView;
+   NSWindow* window;
+   MyOpenGLView* glView;
 }
 @end
 @implementation AppDelegate
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Create the window
-    window = [[NSWindow alloc]
-        initWithContentRect:NSMakeRect(0, 0, 640, 480)
-        styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|NSWindowStyleMaskMiniaturizable|NSWindowStyleMaskResizable
-        backing:NSBackingStoreBuffered
-        defer:NO];
-    [window setTitle:@"OpenGL Window"];
+   // Create the window
+   window = [[NSWindow alloc]
+      initWithContentRect:NSMakeRect(0, 0, 640, 480)
+      styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|NSWindowStyleMaskMiniaturizable|NSWindowStyleMaskResizable
+      backing:NSBackingStoreBuffered
+      defer:NO];
+   [window setTitle:@"OpenGL Window"];
 
-    // Create the OpenGL view
-    NSOpenGLPixelFormatAttribute attrs[] = {
-        NSOpenGLPFADoubleBuffer,
-        NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersionLegacy,
-        NSOpenGLPFAColorSize, 24,
-        NSOpenGLPFAAlphaSize, 8,
-        NSOpenGLPFADepthSize, 24,
-        NSOpenGLPFAStencilSize, 8,
-        0
-    };
-    NSOpenGLPixelFormat* pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
-    glView = [[MyOpenGLView alloc] initWithFrame:NSMakeRect(0, 0, 640, 480) pixelFormat:pixelFormat];
-    [window setContentView:glView];
+   // Create the OpenGL view
+   NSOpenGLPixelFormatAttribute attrs[] = {
+      NSOpenGLPFADoubleBuffer,
+      NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersionLegacy,
+      NSOpenGLPFAColorSize, 24,
+      NSOpenGLPFAAlphaSize, 8,
+      NSOpenGLPFADepthSize, 24,
+      NSOpenGLPFAStencilSize, 8,
+      0
+   };
+   NSOpenGLPixelFormat* pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
+   glView = [[MyOpenGLView alloc] initWithFrame:NSMakeRect(0, 0, 640, 480) pixelFormat:pixelFormat];
+   [window setContentView:glView];
 
-    // Show the window
-    [window makeKeyAndOrderFront:nil];
-    [window center];
+   // Show the window
+   [window makeKeyAndOrderFront:nil];
+   [window center];
 }
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
-    return YES;
+   return YES;
 }
 @end
 
 void open_window(int scale){
-    [NSAutoreleasePool new];
-    NSApplication* app = [NSApplication sharedApplication];
-    [app setActivationPolicy:NSApplicationActivationPolicyRegular];
-    [app activateIgnoringOtherApps:YES];
-    AppDelegate* appDelegate = [[AppDelegate alloc] init];
-    [app setDelegate:appDelegate];
-    [app activateIgnoringOtherApps:true];
-    [app run];
+   [NSAutoreleasePool new];
+   NSApplication* app = [NSApplication sharedApplication];
+   [app setActivationPolicy:NSApplicationActivationPolicyRegular];
+   [app activateIgnoringOtherApps:YES];
+   AppDelegate* appDelegate = [[AppDelegate alloc] init];
+   [app setDelegate:appDelegate];
+   [app activateIgnoringOtherApps:true];
+   [app run];
 }
 
 #if defined(MAC_OS_X_VERSION_10_12) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12
