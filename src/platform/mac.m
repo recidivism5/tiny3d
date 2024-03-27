062afa4847823b0185c70f6ecfdccfc724725662
mf <fucker@pornhub.com>
2024-03-27 12:37:44 -0400

    idk

--- src/platform/mac.m
+++ src/platform/mac.m
@@ -3,24 +3,78 @@
 #import <Cocoa/Cocoa.h>
 #import <OpenGL/OpenGL.h>
 #import <OpenGL/gl.h>
+#import <CoreVideo/CVDisplayLink.h>
+
+#include <time.h>
+uint64_t get_time(void){
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (uint64_t) ts.tv_sec * 1000000000 + (uint64_t) ts.tv_nsec;
+}
 
 @interface MyOpenGLView : NSOpenGLView
 @end
 @implementation MyOpenGLView
 - (void)drawRect:(NSRect)dirtyRect {
+   static uint64_t tstart, t0, t1;
+   static GLuint texture = 0;
+   if (!texture){
+      tstart = get_time();
+      t0 = tstart;
+
+      glGenTextures(1,&texture);
+      glBindTexture(GL_TEXTURE_2D,texture);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   }
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_BGRA, GL_UNSIGNED_BYTE, screen);
+
+   t1 = get_time();
+   update((double)(t1-tstart) / 1000000000.0, (double)(t1-t0) / 1000000000.0);
+   t0 = t1;
+   
    //https://stackoverflow.com/questions/2440089/nsopenglview-resize-on-window-resize
-   glViewport(0,0,(int)[[self superview] bounds].size.width,(int)[[self superview] bounds].size.height);
-   glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+   int width = (int)[[self superview] bounds].size.width;
+   int height = (int)[[self superview] bounds].size.height;
+   glViewport(0,0,width,height);
+   
+   glClearColor(0.0f,0.0f,1.0f,1.0f);
    glClear(GL_COLOR_BUFFER_BIT);
+
+   int scale = 1;
+   while (SCREEN_WIDTH*scale <= width && SCREEN_HEIGHT*scale <= height){
+      scale++;
+   }
+   scale--;
+   int scaledWidth = scale * SCREEN_WIDTH;
+   int scaledHeight = scale * SCREEN_HEIGHT;
+   int x = width/2-scaledWidth/2;
+   int y = height/2-scaledHeight/2;
+   glViewport(x,y,scaledWidth,scaledHeight);
+   glEnable(GL_TEXTURE_2D);
    glBegin(GL_QUADS);
-   glVertex2f(0,0);
-   glVertex2f(1,0);
-   glVertex2f(1,1);
-   glVertex2f(0,1);
+   glTexCoord2f(0,0); glVertex2f(-1,-1);
+   glTexCoord2f(1,0); glVertex2f(1,-1);
+   glTexCoord2f(1,1); glVertex2f(1,1);
+   glTexCoord2f(0,1); glVertex2f(-1,1);
    glEnd();
+
    [[self openGLContext] flushBuffer];
 }
 @end
+
+CVReturn displayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp *inNow, const CVTimeStamp *inOutputTime, CVOptionFlags flagsIn, CVOptionFlags *flagsOut, void *displayLinkContext)
+{
+   NSAutoreleasePool* pool = [NSAutoreleasePool new];
+    if ([self lockFocusIfCanDraw])
+    {
+        //Your drawing code
+        [self unlockFocus];
+    }
+    [pool drain];
+
+    return kCVReturnSuccess;
+}
 
 @interface AppDelegate : NSObject <NSApplicationDelegate> {
    NSWindow* window;
@@ -54,6 +108,16 @@
    // Show the window
    [window makeKeyAndOrderFront:nil];
    [window center];
+
+   GLint swapInt = 1;
+   CVDisplayLinkRef displayLink;
+   [[glView openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval]; 
+   CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+   CVDisplayLinkSetOutputCallback(displayLink, &displayLinkCallback, self);
+   CGLContextObj cglContext = [[glView openGLContext] CGLContextObj];
+   CGLPixelFormatObj cglPixelFormat = [[NSOpenGLView defaultPixelFormat] CGLPixelFormatObj];
+   CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
+   CVDisplayLinkStart(displayLink);
 }
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
    return YES;
